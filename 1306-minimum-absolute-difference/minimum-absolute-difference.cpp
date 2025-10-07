2e08a080cafddf4ab05ce76fb475d68792e30eaf
Aman Srivastava <145239544+its-amans@users.noreply.github.com>
2025-10-07 17:01:20 +0530

    Time: 15 ms (74.13%) | Memory: 36.7 MB (32.56%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -5,16 +5,14 @@ public:
         
         int n=arr.size();
         int absdiff=INT_MAX;
-        for(int i=0;i<n-1;i++){
-            absdiff=min(absdiff,abs(arr[i+1]-arr[i]));
+        for(int i=1;i<n;i++){
+            absdiff=min(absdiff,abs(arr[i]-arr[i-1]));
         }
         
         vector<vector<int>> ans;
-        for(int i=0;i<n-1;i++){
-            if(abs(arr[i+1]-arr[i])==absdiff){
-                vector<int> nw={arr[i+1],arr[i]};
-                sort(nw.begin(),nw.end());
-                ans.push_back(nw);
+        for(int i=1;i<n;i++){
+            if(abs(arr[i]-arr[i-1])==absdiff){
+                ans.push_back({arr[i-1],arr[i]});
             }
 
         }

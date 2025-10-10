e9c03204a90a416a4ee7f872e2cef83adc177e8f
Aman Srivastava <145239544+its-amans@users.noreply.github.com>
2025-10-11 02:35:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (38.34%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> mp;
-        int ans;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+        
+        int a=0,b=0;
+        for(auto num:nums)
+        {
+            a=(a^num)&~b;
+            b=(b^num)&~a;
         }
-
-        for(int i=0;i<n;i++){
-            if(mp[nums[i]]==1){
-                ans= nums[i];
-            }
-        }
-        return ans;
+        return a;
     }
 };

bdf690972e262e077de637d965194bdd35ad8898
Aman Srivastava <145239544+its-amans@users.noreply.github.com>
2025-11-27 22:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (50.63%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,24 +2,13 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
     //If the no has only one setBit then it is in power of 2
-        int bitCount=0;
 
         if(n<=0){
             return false;
         }
-        while(n){
-            int bit=n&1;
-
-            if(bit){
-                bitCount++;
-            }
-            n>>=1;
-        }
-
-        if(bitCount==1){
+        if((n&(n-1))==0){
             return true;
         }
         return false;
-
     }
 };
